4cd7024bd8b6abd7b10ef333bfbdf4ebc7ce6c27
04gab <106406199+04gab@users.noreply.github.com>
2024-07-27 13:14:54 -0700

    yrd

---
+++ project tank/object_detection_CV.py
@@ -0,0 +1,8 @@
+#%%
+import torch
+import torchvision
+print(torch.__version__)
+print(torchvision.__version__)
+# computer vision
+# ----------------------------------------------------------------------------------------------------------
+#%%
