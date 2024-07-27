2f2e04f2f3959c2a188c53033cfcca05351f012c
04gab <106406199+04gab@users.noreply.github.com>
2024-07-27 16:02:03 -0700

    moved files

--- project tank/object_detection_CV.py
+++ project tank/object_detection_CV.py
@@ -1,8 +1,18 @@
 #%%
 import torch
 import torchvision
+import os
+from os import listdir
 print(torch.__version__)
 print(torchvision.__version__)
 # computer vision
 # ----------------------------------------------------------------------------------------------------------
 #%%
+print(os.listdir())
+folder_dir = "1"
+i=0
+for images in os.listdir(folder_dir):
+    if images.endswith(".png"):
+        print(f"image number {i}")
+        i+=1
+# %%
