d936906bb5c2fbf3268ef60ebcd19e32d162485a
04gab <106406199+04gab@users.noreply.github.com>
2024-07-27 19:47:55 -0700

    deleated

--- project tank/object_detection_CV.py
+++ project tank/object_detection_CV.py
@@ -1,18 +1,13 @@
 #%%
 import torch
 import torchvision
+from torchvision import datasets, transforms
+from PIL import Image
+import pathlib as Path
 import os
 from os import listdir
 print(torch.__version__)
 print(torchvision.__version__)
 # computer vision
 # ----------------------------------------------------------------------------------------------------------
-#%%
-print(os.listdir())
-folder_dir = "1"
-i=0
-for images in os.listdir(folder_dir):
-    if images.endswith(".png"):
-        print(f"image number {i}")
-        i+=1
-# %%
+
