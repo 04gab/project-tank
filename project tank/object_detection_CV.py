fb7056e062a328e4bf0dea7e077e20ef15d15a6a
04gab <106406199+04gab@users.noreply.github.com>
2024-07-27 23:53:54 -0700

    sadge

--- project tank/object_detection_CV.py
+++ project tank/object_detection_CV.py
@@ -1,13 +1,10 @@
 #%%
-import torch
-import torchvision
-from torchvision import datasets, transforms
-from PIL import Image
-import pathlib as Path
-import os
-from os import listdir
+import torch 
+from torch import nn
 print(torch.__version__)
-print(torchvision.__version__)
 # computer vision
 # ----------------------------------------------------------------------------------------------------------
-
+# %%
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+model
+# %%
